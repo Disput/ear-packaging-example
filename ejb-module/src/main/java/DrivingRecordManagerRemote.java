e844696af21ebdf77728e945811fa5cbfade6652
Alexander Haase <alexander.haase@de.ibm.com>
2020-10-22 12:47:40 +0200

    new test content

--- ejb-module/src/main/java/DrivingRecordManagerRemote.java
+++
@@ -1,8 +0,0 @@
-import javax.ejb.EJBException;
-import javax.ejb.EJBObject;
-import java.rmi.RemoteException;
-
-public interface DrivingRecordManagerRemote extends EJBObject {
-
-    public String doSomethingOnServer(final String command) throws EJBException, RemoteException;
-}
---
+++ ejb-module/src/main/java/ImplExample.java
@@ -0,0 +1,5 @@
+public class ImplExample implements Hello {
+    public void printMsg() {
+        System.out.println("this is an exampe rmi programm");
+    }
+}
